7a8d986aa8953039ca6e357dcd952b786e0fb117
Jinal <jpatel111102@gmail.com>
2026-04-16 20:25:16 +0530

    Portfolio

--- src/Component/Projects.tsx
+++ src/Component/Projects.tsx
@@ -9,10 +9,10 @@ const Projects = () => {
                     <p className="dark:text-yellow-300 text-[#098acc] underline">Projects</p>
 
                 </div>
-                <div className="grid sm:grid-cols-2 justify-center items-center gap-6 ">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-7 ">
 
                     {/* Project 1 */}
-                    <div className="dark:bg-[#0b0f1a] border border-slate-800 rounded-2xl p-6  bg-gradient-to-r from-[#3a0425] to-[#d387b6] dark:bg-gradient-to-r dark:from-[#314755] dark:to-[#26a0da] mt-7 w-72 ml-5">
+                    <div className="dark:bg-[#0b0f1a] border border-slate-800 rounded-2xl p-6  bg-gradient-to-r from-[#3a0425] to-[#d387b6] dark:bg-gradient-to-r dark:from-[#314755] dark:to-[#26a0da] mt-7">
 
                         <h2 className="text-xl font-bold  dark:text-white text-yellow-200">
                             Jewellery
@@ -50,7 +50,7 @@ const Projects = () => {
                     </div>
 
                     {/* Project 2 */}
-                    <div className="dark:bg-[#0b0f1a] border border-slate-800 rounded-2xl  bg-gradient-to-r from-[#3a0425] to-[#d387b6] dark:bg-gradient-to-r dark:from-[#314755] dark:to-[#26a0da] mt-7 w-72">
+                    <div className="dark:bg-[#0b0f1a] border border-slate-800 rounded-2xl  bg-gradient-to-r from-[#3a0425] to-[#d387b6] dark:bg-gradient-to-r dark:from-[#314755] dark:to-[#26a0da] mt-7 ">
 
                         <h2 className="text-xl font-bold  dark:text-white text-yellow-200">
                             Country
